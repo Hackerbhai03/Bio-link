12191f232fc6d0856c578f9fbba683353c5f4b43
𝐇𝐀𝐂𝐊𝐄𝐑 <ayushprincekmr03@gmail.com>
2026-02-19 01:06:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,8 +13,8 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
 
 # -----------------------------------------------
 
-OWNER_ID = int(os.environ.get("OWNER_ID", "8056154987"))
-SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "8056154987"))
+OWNER_ID = int(os.environ.get("OWNER_ID", "7682307978"))
+SPECIAL_ID = int(os.environ.get("SPECIAL_ID", "7682307978"))
 # ------------------------------------------------
 
 # ------------------------------------------------
